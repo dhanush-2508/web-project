e52c363685c006363e0d5bd006e981c871fa4fa9
dhanush <dhanushgunuganti@gmail.com>
2026-08-14 10:58:35 +0530

    added js

--- frontend/index.js
+++ frontend/index.js
@@ -24,3 +24,21 @@ function toggle(){
  document.getElementById("card-name").innerText =users[curIndex].name
  document.getElementById("card-gender").innerText =users[curIndex].gender
 }
+
+
+function random() {
+    fetch('https://randomuser.me/api')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            var detail = data.results[0];
+
+            document.getElementById("card-image").src = detail.picture.large;
+            document.getElementById("card-gender").innerText = detail.gender;
+
+            var fullname = detail.name.title + " " +detail.name.first + " " + detail.name.last;
+            document.getElementById("card-name").innerText = fullname;
+        });
+}
+
